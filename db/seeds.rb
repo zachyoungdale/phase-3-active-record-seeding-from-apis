f53a249d2399bc21c5c0047d143db859f5586a32
Zachary Youngdale <zyoungdale@gmail.com>
2022-10-17 12:16:26 -0400

    commit tree d7ac93c17f14500a49c7f0fb8b671eae2c13e3e3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,18 @@
 # Add seed data in this file
+puts "Seeding spells..."
+
+spells = ["acid-arrow", "animal-messenger", "calm-emotions", "charm-person"]
+
+spells.each do |spell| 
+    response = RestClient.get "https://www.dnd5eapi.co/api/spells/#{spell}"
+
+    spell_hash = JSON.parse(response)
+
+    Spell.create(
+        name: spell_hash["name"],
+        level: spell_hash["level"],
+        description: spell_hash["desc"][0]
+    )
+end
+
+puts "Done seeding"
